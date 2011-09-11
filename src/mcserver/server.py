c700dea9d33a56ce5c4861b9a88694598730af23
Michael Daffin <james1479@gmail.com>
2011-09-11 14:34:54 +0100

    Added a first run method and subprocess calls

--- src/mcserver/server.py
+++ src/mcserver/server.py
@@ -1,10 +1,49 @@
-import os
+import os, errno
+import sys
+import subprocess
 
 class Server:
-    pass
     
-    def start(self):
-        pass
+    """
+    Starts the minecraft server
+    """
+    def start( self ):
+        self.first_run()
 
-    def stop(self):
-        pass
+        subprocess.Popen( 'sleep 20 && echo done', shell=True )
+        while True:
+            try:
+                input = sys.stdin.readline()
+                if input:
+                    self.send(input)
+            except KeyboardInterrupt:
+                self.stop()
+
+    """
+    Checks for the first run and sets up anything that needs to be set up
+    """
+    def first_run( self ):
+        directory = os.getcwd() + '/minecraft'
+
+        try:
+            os.mkdir( directory )
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    """
+    Stops the minecraft server
+    """
+    def stop( self, value=0 ):
+        sys.stdout.write( 'Shutting down server...' )
+        sys.exit( value )
+
+    """
+    Sends a command to the minecraft server
+    """
+    def send( self, message ):
+        sys.stdout.write( message )
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
