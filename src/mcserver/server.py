13c25b4708033f45320f0548a9a66d72916fb9fb
Michael Daffin <james@nemo.localdomain>
2011-09-13 12:22:19 +0100

    Added a check to see if the server is running, added a quit function and a some checks before starting the server

--- src/mcserver/server.py
+++ src/mcserver/server.py
@@ -5,16 +5,25 @@ import subprocess
 class Server:
 
     def __init__( self ):
-        pass
-
+        self.arguments = 'nogui'
+        self.server_jar = 'minecraft_server.jar'
+        self.running = False
     
     """
     Starts the minecraft server
     """
     def start( self ):
+        if self.running:
+            sys.stderr.write( 'Error: Server already running' )
+            return
+
         self.first_run()
 
-        self._mcp = subprocess.Popen( 'java -jar minecraft_server.jar', shell=True )
+        if !check_jar():
+            return
+
+        self._mcp = subprocess.Popen( 'java ' + self.arguments + ' -jar ' + self.server_jar, shell=True )
+        self.running = True
         while True:
             try:
                 input = sys.stdin.readline()
@@ -45,10 +54,17 @@ class Server:
     """
     Stops the minecraft server
     """
-    def stop( self, value=0 ):
+    def stop( self ):
         sys.stdout.write( 'Shutting down server...' )
         self._mcp.communicate( 'stop' )
         self._mcp.wait()
+        self.running = False
+
+    """
+    Stops the minecraft server then exits
+    """
+    def quit( self, value=0):
+        stop()
         sys.exit( value )
 
     """
@@ -56,6 +72,20 @@ class Server:
     """
     def send( self, message ):
         self._mcp.communicate( message )
+
+    def check_jar():
+        if !os.path.exists( self.server_jar ):
+            sys.stderr.write( 'Error: ' + self.server_jar + ' does not exist' )
+            return false
+        return true
+
+    def process_input( input ):
+        if input == 'stop':
+            stop()
+        elif input == 'quit':
+            quit()
+        else:
+            send( input )
 
 if __name__ == "__main__":
     server = Server()
