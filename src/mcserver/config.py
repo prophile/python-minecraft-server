e72f6f879a06d43958156ccdc747d98fd576429d
Michael Daffin <james1479@gmail.com>
2011-09-17 16:55:24 +0100

    Fixed the urls for bukkit

--- src/mcserver/config.py
+++ src/mcserver/config.py
@@ -1,7 +1,8 @@
 java_exec = 'java'
 jar_bin = './minecraft'
 java_flags = '-Xms1G -Xmx2G'
+server_type = 'bukkit dev'
 url = dict()
 url['offical'] = 'http://www.minecraft.net/download/minecraft_server.jar'
-url['bukkit'] = 'http://www.minecraft.net/download/minecraft_server.jar'
-url['bukkit dev'] = 'http://www.minecraft.net/download/minecraft_server.jar'
+url['bukkit'] = 'http://ci.bukkit.org/job/dev-CraftBukkit/Recommended/artifact/target/craftbukkit-0.0.1-SNAPSHOT.jar'
+url['bukkit dev'] = 'http://ci.bukkit.org/job/dev-CraftBukkit/lastStableBuild/artifact/target/craftbukkit-0.0.1-SNAPSHOT.jar'
