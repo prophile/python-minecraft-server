fbb47b7832e58c02b53e6d1ecb3f49f7ea2597be
Michael Daffin <james1479@gmail.com>
2011-10-17 12:35:13 +0100

    Added a basic setup.py script
    This makes it easier to install the module like any other python program

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PythonCraft',
+    version='0.1a',
+    description='A minecraft server wrapper to add extra admin functions',
+    author='Michael Daffin',
+    author_email='james1479@gmail.com',
+    url='',
+    packages=find_packages()
+)
